d3c25b9152ec27fa8540f9c749cb8d40d2bf2189
JeanCarlos911 <jeancarlosodi@gmail.com>
2020-10-02 14:48:15 -0500

    se reduce ruido visual

--- otros/operaciones-elementales/for-anidado-con-if/src/Algoritmo.java
+++ otros/operaciones-elementales/for-anidado-con-if/src/Algoritmo.java
@@ -25,7 +25,6 @@ public class Algoritmo {
             = n^2 - 2n + 1 - (n - 1)(n - 2)/2
             = (n^2 - n) / 2
 
-
          resultado final: ((n^2 - n) / 2)*10 + 4*(n-1) + 2
             = ((n^2 - n) / 2)*10 + 4*n - 2
 
--- otros/operaciones-elementales/for-anidado-con-if/src/Main.kt
+++ otros/operaciones-elementales/for-anidado-con-if/src/Main.kt
@@ -3,22 +3,21 @@ import kotlin.random.Random
 fun main() {
     val n = 100
     var a: IntArray
+    val formulaCaso1: Int
+    val formulaCaso2: Int
 
     //caso 1, descendente 100, 99, ..., 2, 1 siempre ejecuta las instrucciones del if
-    a = IntArray(n) {100 - it}
-    println("---------- Caso 1 ---------- \nPor contador: ${Algoritmo.calcular(n, a)}")
-    val caso1 = 10*((n*n - n )/ 2) + 4*n - 2
-    println("por fórmula: $caso1")
+    a = IntArray(n) {n - it}
+    formulaCaso1 = 10*((n*n - n )/ 2) + 4*n - 2
+    println("---------- Caso 1 ---------- \nPor contador: ${Algoritmo.calcular(n, a)}\nPor fórmula: $formulaCaso1")
 
     //caso 2, ascendente 0, 1, 2, ..., 98, 99 nunca ejecuta las instrucciones del if
     a = IntArray(n) {it}
-    println("---------- Caso 2 ---------- \nPor contador: ${Algoritmo.calcular(n, a)}")
-    val caso2 = 5*((n*n - n )/ 2) + 4*n - 2
-    println("por fórmula: $caso2")
+    formulaCaso2 = 5*((n*n - n )/ 2) + 4*n - 2
+    println("---------- Caso 2 ---------- \nPor contador: ${Algoritmo.calcular(n, a)}\nPor fórmula: $formulaCaso2")
 
     //caso 3, random 4, 5, 2, 6, 7, 4, 5, 8, 9 las ejecuta la mitad de las veces.
-    a = IntArray(n) {Random.nextInt(0, 100)}
-    println("---------- Caso 3 ---------- \nPor contador: ${Algoritmo.calcular(n, a)}")
-    println("Estimación:  ${(caso2 + caso1) / 2}")
+    a = IntArray(n) {Random.nextInt(0, n)}
+    println("---------- Caso 3 ---------- \nPor contador: ${Algoritmo.calcular(n, a)}\nEstimación:  ${(formulaCaso2 + formulaCaso1) / 2}")
 
 }
