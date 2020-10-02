76397b38c17dfcde15920c8d6af1a03c605df5d9
JeanCarlos911 <jeancarlosodi@gmail.com>
2020-10-02 12:20:56 -0500

    se ha adaptado para justificar

--- otros/operaciones-elementales/for-anidado-con-if/src/Algoritmo.java
+++ otros/operaciones-elementales/for-anidado-con-if/src/Algoritmo.java
@@ -2,12 +2,52 @@ public class Algoritmo {
     public static int calcular(int n, int[] a) {
         int t;
         int c = 0;
+/* PROCEDIMIENTO: Observar y determinar comportamiento para un n particular (n=100) y generalizar
+        i        | j        | intervalo    | se  repite
+        -----------------------------------------------
+        0         1          [1, 100)        99 veces
+        1         2          [2, 100)        98 veces
+        2         3          [3, 100)        97 veces
+        n - 3     n - 2      [n - 1, n)      2  veces
+        n - 2     n - 1      [n - 1, n)      1  vez
+        n - 1     n          [n, n)          0  veces
 
-        for(int i=0; i < n-1; i++) { // (5x + 5*CASO1) + 4*(n-1) + 2
+        fórmula: (n - 1) - (i + 1) + 1
+
+        fórmula simplificada: n - 1 - i
+            prueba de la fórmula:
+            i = 0    :      n - 1 - (0)     = 99
+            i = n - 2:      n - 1 - (n - 2) = 1
+            i = n - 1:      n - 1 - (n - 1) = 0
+*/
+/*      sumatoria desde i = 0 hasta n - 2 de n - 1 - i
+            = n(n - 1) - (n - 1) - (n - 1)(n - 2)/2
+            = n^2 - 2n + 1 - (n - 1)(n - 2)/2
+            = (n^2 - n) / 2
+
+
+         resultado final: ((n^2 - n) / 2)*10 + 4*(n-1) + 2
+            = ((n^2 - n) / 2)*10 + 4*n - 2
+
+         evaluado en n=100: ((10000 - 100) / 2)*10 + 400 - 2 = 49898
+*/
+/*      f = (n^2 - n) / 2
+            CASO 1 ->  10*f + 4*n - 2
+            CASO 2 ->  5*f + 4*n - 2
+            CASO 3 -> (caso1 - caso2) / 2
+
+        RESULTADO GENERALIZADO: (5x + 5)*f + 4*n - 2
+
+        x = when(CASO){
+            1 -> 1
+            2 -> 0
+        }
+ */
+        for(int i=0; i < n-1; i++) { // (5x + 5)*f + 4*n - 2
             c+=2;
-            for(int j = i + 1; j < n; j++) { // 5x + 5*CASO1 + 2, para n - 1 repeticiones
+            for(int j = i + 1; j < n; j++) { // (5x + 5)*f + 2
                 c+=2;
-                if(a[i] > a[j]) {// 5x, donde x depende del caso
+                if(a[i] > a[j]) {// 5x
                     t    = a[i]; //2
                     a[i] = a[j]; //3
                     a[j] = t;    //2
@@ -21,11 +61,4 @@ public class Algoritmo {
 
         return c;
     }
-
-    /*
-    - - - - - - - - - - - - Valor de x - - - - - - - - - - - - - -
-    CASO 1: (n *(n - 1) / 2)
-    CASO 2: 0
-    CASO 3: sólo se puede estimar con Random.nextInt(cCASO2, cCASO1)
-     */
 }
--- otros/operaciones-elementales/for-anidado-con-if/src/Main.kt
+++ otros/operaciones-elementales/for-anidado-con-if/src/Main.kt
@@ -7,13 +7,13 @@ fun main() {
     //caso 1, descendente 100, 99, ..., 2, 1 siempre ejecuta las instrucciones del if
     a = IntArray(n) {100 - it}
     println("---------- Caso 1 ---------- \nPor contador: ${Algoritmo.calcular(n, a)}")
-    val caso1 = (10*(n *(n - 1) / 2)) + 4*(n-1) + 2
+    val caso1 = 10*((n*n - n )/ 2) + 4*n - 2
     println("por fórmula: $caso1")
 
     //caso 2, ascendente 0, 1, 2, ..., 98, 99 nunca ejecuta las instrucciones del if
     a = IntArray(n) {it}
     println("---------- Caso 2 ---------- \nPor contador: ${Algoritmo.calcular(n, a)}")
-    val caso2 = (5*(n *(n - 1) / 2)) + 4*(n-1) + 2
+    val caso2 = 5*((n*n - n )/ 2) + 4*n - 2
     println("por fórmula: $caso2")
 
     //caso 3, random 4, 5, 2, 6, 7, 4, 5, 8, 9 las ejecuta la mitad de las veces.
